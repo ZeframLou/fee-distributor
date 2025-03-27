423c45c37cb8e257a188aee7fb18991bc57c1984
ZeframLou <zeframlou@gmail.com>
2025-03-27 15:41:32 -0700

    build: update libraries, update deploy script & test

--- script/DeployMainnet.s.sol
+++ script/DeployMainnet.s.sol
@@ -1,26 +1,29 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.0;
 
-import {CREATE3Factory} from "create3-factory/src/CREATE3Factory.sol";
-
 import "forge-std/Script.sol";
 
+import {CREATE3Script} from "./base/CREATE3Script.sol";
 import {FeeDistributor} from "../src/FeeDistributor.sol";
 
-contract DeployScript is Script {
-    function run() external returns (FeeDistributor deployed) {
+contract DeployScript is CREATE3Script {
+    constructor() CREATE3Script(vm.envString("VERSION")) {}
+
+    function run() external returns (FeeDistributor deployed, bytes32 salt) {
+        // Select mainnet fork
+        vm.createSelectFork("mainnet");
+
         uint256 deployerPrivateKey = uint256(vm.envBytes32("PRIVATE_KEY"));
-        CREATE3Factory create3 = CREATE3Factory(0x9fBB3DF7C40Da2e5A0dE984fFE2CCB7C47cd0ABf);
+
         address votingEscrow = vm.envAddress("VOTING_ESCROW_MAINNET");
         uint256 startTime = vm.envUint("START_TIME_MAINNET");
+
+        salt = getCreate3SaltFromEnv("FeeDistributor");
 
         vm.startBroadcast(deployerPrivateKey);
 
         deployed = FeeDistributor(
-            create3.deploy(
-                keccak256("FeeDistributor-veLIT"),
-                bytes.concat(type(FeeDistributor).creationCode, abi.encode(votingEscrow, startTime))
-            )
+            create3.deploy(salt, bytes.concat(type(FeeDistributor).creationCode, abi.encode(votingEscrow, startTime)))
         );
 
         vm.stopBroadcast();
--- test/Test.sol
+++ test/Test.sol
@@ -4,13 +4,21 @@ pragma solidity ^0.8.11;
 import "forge-std/Test.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {FeeDistributor} from "../src/FeeDistributor.sol";
+import {IVotingEscrow} from "../src/interfaces/IVotingEscrow.sol";
 
 contract FeeDistributorTest is Test {
-    FeeDistributor constant f = FeeDistributor(0x951f99350d816c0E160A2C71DEfE828BdfC17f12);
+    FeeDistributor public f;
     ERC20 constant weth = ERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2);
+    IVotingEscrow constant votingEscrow = IVotingEscrow(0x00000042877f4a1cC0693383ebdAc7c0e0A1bf77);
     address constant user = 0x5f350bF5feE8e254D6077f8661E9C7B83a30364e;
 
-    function setUp() external {}
+    function setUp() external {
+        // Select mainnet fork
+        vm.createSelectFork("mainnet");
+
+        // Deploy FeeDistributor with current timestamp as start time
+        f = new FeeDistributor(votingEscrow, block.timestamp);
+    }
 
     function test_distribute() external {
         // distribute WETH
@@ -19,14 +27,8 @@ contract FeeDistributorTest is Test {
         weth.approve(address(f), amount);
         f.depositToken(weth, amount);
 
-        // wait for 1 hour
-        skip(1 hours);
-
-        // check claimable amount
-        assertEq(f.claimToken(user, weth), 0, "claimed WETH");
-
-        // wait for 10 hours
-        skip(10 hours);
+        // wait for 1 week
+        skip(1 weeks);
 
         // check claimable amount
         assertTrue(f.claimToken(user, weth) > 0, "didn't claim WETH");
